f4d6c39250f461dfae3ab3f94d54ea78507231a3
kqke <carl.veksler@mail.huji.ac.il>
2019-09-16 13:46:05 +0300

    firebase UI login integration

--- app/src/main/java/com/example/kit/ui/LoginActivity.java
+++ app/src/main/java/com/example/kit/ui/LoginActivity.java
@@ -1,14 +1,10 @@
 package com.example.kit.ui;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -20,10 +16,8 @@ import com.example.kit.models.User;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -32,8 +26,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
 import java.util.ArrayList;
-
-import static android.text.TextUtils.isEmpty;
 
 public class LoginActivity extends AppCompatActivity
 //        implements
@@ -44,15 +36,11 @@ public class LoginActivity extends AppCompatActivity
 
 //    //Firebase
     private FirebaseAuth.AuthStateListener mAuthListener;
-//
-//    // widgets
-//    private EditText mEmail, mPassword;
+
     private ProgressBar mProgressBar;
-//
-//    Button signInButton;
+
     private FirebaseAuth mAuth;
     public static final int RC_SIGN_IN = 900;
-    protected ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +49,6 @@ public class LoginActivity extends AppCompatActivity
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
         setupFirebaseAuth();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
         mProgressBar = findViewById(R.id.progressBar);
         showDialog();
     }
