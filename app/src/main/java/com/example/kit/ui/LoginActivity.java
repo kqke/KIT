6bb67e38c6b537d7f30e98fad547f142bef35efd
kqke <carl.veksler@mail.huji.ac.il>
2019-09-16 13:44:05 +0300

    firebase UI login integration

--- app/src/main/java/com/example/kit/ui/LoginActivity.java
+++ app/src/main/java/com/example/kit/ui/LoginActivity.java
@@ -43,11 +43,11 @@ public class LoginActivity extends AppCompatActivity
     private static final String TAG = "LoginActivity";
 
 //    //Firebase
-//    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 //
 //    // widgets
 //    private EditText mEmail, mPassword;
-//    private ProgressBar mProgressBar;
+    private ProgressBar mProgressBar;
 //
 //    Button signInButton;
     private FirebaseAuth mAuth;
@@ -60,23 +60,10 @@ public class LoginActivity extends AppCompatActivity
         setContentView(R.layout.activity_login);
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
+        setupFirebaseAuth();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser == null)
-        {
-            startAuthenticationActivity();
-        }
-
-        startMainActivity();
-//        signInButton = (Button) findViewById(R.id.button);
-//        mEmail = findViewById(R.id.email);
-//        mPassword = findViewById(R.id.password);
-//        mProgressBar = findViewById(R.id.progressBar);
-//
-//        setupFirebaseAuth();
-//        findViewById(R.id.email_sign_in_button).setOnClickListener(this);
-//        findViewById(R.id.link_register).setOnClickListener(this);
-//
-//        hideSoftKeyboard();
+        mProgressBar = findViewById(R.id.progressBar);
+        showDialog();
     }
 
     public void startAuthenticationActivity () {
@@ -120,154 +107,72 @@ public class LoginActivity extends AppCompatActivity
         finish();
     }
 
-//    public void login_click (View v) {
-//        startAuthenticationActivity();
-//    }
 
+    private void showDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
 
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        authenticateWithCachedToken();
-//    }
-//
-//    protected void authenticateWithCachedToken () {
-//        signInButton.setEnabled(false);
-//
-//        ChatSDK.auth().authenticate()
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .doFinally(() -> {
-//                    signInButton.setEnabled(true);
-//                })
-//                .subscribe(() -> {
-//                    ChatSDK.ui().startMainActivity(MainActivity.this);
-//                }, throwable -> {
-//                    // Setup failed
-//                });
-//    }
-
-
-//    private void showDialog(){
-//        mProgressBar.setVisibility(View.VISIBLE);
-//
-//    }
-//
-//    private void hideDialog(){
-//        if(mProgressBar.getVisibility() == View.VISIBLE){
-//            mProgressBar.setVisibility(View.INVISIBLE);
-//        }
-//    }
-//
-//    private void hideSoftKeyboard(){
-//        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-//    }
+    }
 
     /*
         ----------------------------- Firebase setup ---------------------------------
      */
-//    private void setupFirebaseAuth(){
-//        Log.d(TAG, "setupFirebaseAuth: started.");
-//
-//        mAuthListener = new FirebaseAuth.AuthStateListener() {
-//            @Override
-//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-//                FirebaseUser user = firebaseAuth.getCurrentUser();
-//                if (user != null) {
-//                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-//                    Toast.makeText(LoginActivity.this, "Authenticated with: " + user.getEmail(), Toast.LENGTH_SHORT).show();
-//
-//                    FirebaseFirestore db = FirebaseFirestore.getInstance();
-//                    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-//                            .setTimestampsInSnapshotsEnabled(true)
-//                            .build();
-//                    db.setFirestoreSettings(settings);
-//
-//                    DocumentReference userRef = db.collection(getString(R.string.collection_users))
-//                            .document(user.getUid());
-//
-//                    userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//                        @Override
-//                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                            if(task.isSuccessful()){
-//                                Log.d(TAG, "onComplete: successfully set the user client.");
-//                                User user = task.getResult().toObject(User.class);
-//                                System.out.println("yadadada");
-//                                System.out.println(user);
-//                                ((UserClient)(getApplicationContext())).setUser(user);
-//                            }
-//                        }
-//                    });
-//
-//                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//                    startActivity(intent);
-//                    finish();
-//
-//                } else {
-//                    // User is signed out
-//                    Log.d(TAG, "onAuthStateChanged:signed_out");
-//                }
-//                // ...
-//            }
-//        };
-//    }
-//
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//        if (mAuthListener != null) {
-//            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
-//        }
-//    }
-//
-//    private void signIn(){
-//        //check if the fields are filled out
-//        if(!isEmpty(mEmail.getText().toString())
-//                && !isEmpty(mPassword.getText().toString())){
-//            Log.d(TAG, "onClick: attempting to authenticate.");
-//
-//            showDialog();
-//
-//            FirebaseAuth.getInstance().signInWithEmailAndPassword(mEmail.getText().toString(),
-//                    mPassword.getText().toString())
-//                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                        @Override
-//                        public void onComplete(@NonNull Task<AuthResult> task) {
-//
-//                            hideDialog();
-//
-//                        }
-//                    }).addOnFailureListener(new OnFailureListener() {
-//                @Override
-//                public void onFailure(@NonNull Exception e) {
-//                    Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
-//                    hideDialog();
-//                }
-//            });
-//        }else{
-//            Toast.makeText(LoginActivity.this, "You didn't fill in all the fields.", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//    @Override
-//    public void onClick(View view) {
-//        switch (view.getId()){
-//            case R.id.link_register:{
-//                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-//                startActivity(intent);
-//                break;
-//            }
-//
-//            case R.id.email_sign_in_button:{
-//               signIn();
-//               break;
-//            }
-//        }
-//    }
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: started.");
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Toast.makeText(LoginActivity.this, "Authenticated with: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                            .setTimestampsInSnapshotsEnabled(true)
+                            .build();
+                    db.setFirestoreSettings(settings);
+
+                    DocumentReference userRef = db.collection(getString(R.string.collection_users))
+                            .document(user.getUid());
+
+                    userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if(task.isSuccessful()){
+                                Log.d(TAG, "onComplete: successfully set the user client.");
+                                User user = task.getResult().toObject(User.class);
+                                System.out.println("yadadada");
+                                System.out.println(user);
+                                ((UserClient)(getApplicationContext())).setUser(user);
+                            }
+                        }
+                    });
+
+                    startMainActivity();
+
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    startAuthenticationActivity();
+                }
+                // ...
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }
+
 }
