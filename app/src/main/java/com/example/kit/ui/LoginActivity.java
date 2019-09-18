3cb771d6b5d6a41be69eb10bc8730dd5f4ab86bc
kqke <carl.veksler@mail.huji.ac.il>
2019-09-18 17:18:31 +0300

    changes

--- app/src/main/java/com/example/kit/ui/LoginActivity.java
+++ app/src/main/java/com/example/kit/ui/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -13,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.kit.R;
 import com.example.kit.UserClient;
 import com.example.kit.models.User;
+import com.example.kit.util.UsernameValidator;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,29 +23,36 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-import static com.example.kit.Constants.GET_USERNAME_REQUEST;
+import static com.example.kit.Constants.EMPTY;
+import static com.example.kit.Constants.RC_SIGN_IN;
+import static com.example.kit.Constants.WRONG_FORMAT;
+import static com.example.kit.util.Check.isEmpty;
 
 public class LoginActivity extends AppCompatActivity
+        implements View.OnClickListener
 {
-
     private static final String TAG = "LoginActivity";
 
     //Firebase
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseFirestore mDb;
+    private FirebaseAuth mAuth;
 
+    //widgets
+    private EditText mUsername;
     private ProgressBar mProgressBar;
 
-    private FirebaseAuth mAuth;
-    public static final int RC_SIGN_IN = 900;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,9 +60,37 @@ public class LoginActivity extends AppCompatActivity
         setContentView(R.layout.activity_login);
         FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
-        setupFirebaseAuth();
+        mDb = FirebaseFirestore.getInstance();
         mProgressBar = findViewById(R.id.progressBar);
+        setupFirebaseAuth();
         showDialog();
+    }
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        // RC_SIGN_IN is the request code you passed into  startActivityForResult(...) when starting the sign in flow.
+//        if (requestCode == RC_SIGN_IN) {
+//            IdpResponse response = IdpResponse.fromResultIntent(data);
+//
+//            // Successfully signed in
+//            if (resultCode == RESULT_OK) {
+//                // Success
+////                startMainActivity();
+//            }
+//            else {
+//                // Handle Error
+//            }
+//        }
+//    }
+
+    protected void startMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     public void startAuthenticationActivity () {
@@ -67,54 +105,32 @@ public class LoginActivity extends AppCompatActivity
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(idps)
-                        .build(),
-                RC_SIGN_IN);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // RC_SIGN_IN is the request code you passed into  startActivityForResult(...) when starting the sign in flow.
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            // Successfully signed in
-            if (resultCode == RESULT_OK) {
-                // Success
-//                startMainActivity();
-            }
-            else {
-                // Handle Error
-            }
-        }
-        else if (requestCode == GET_USERNAME_REQUEST)
-        {
-            if (resultCode == RESULT_OK) {
-                String userName = data.getData().toString();
-                registerNewUser(userName);
-            }
-            else {
-                // Handle Error
-            }
-        }
-    }
-
-    protected void startMainActivity() {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-        finish();
+                        .build(), RC_SIGN_IN);
     }
 
 
     private void showDialog(){
-        mProgressBar.setVisibility(View.VISIBLE);
 
+        // TODO
+        // We can put here the logo of the app with loading animation or smth.
+
+        mProgressBar.setVisibility(View.VISIBLE);
     }
 
-    /*
-        ----------------------------- Firebase setup ---------------------------------
-     */
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+
+    private void hideDialog(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+/*
+    ----------------------------- Firebase setup ---------------------------------
+ */
     private void setupFirebaseAuth(){
         Log.d(TAG, "setupFirebaseAuth: started.");
 
@@ -123,55 +139,122 @@ public class LoginActivity extends AppCompatActivity
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                    Toast.makeText(LoginActivity.this, "Authenticated with: " + user.getEmail(), Toast.LENGTH_SHORT).show();
-
-                    FirebaseFirestore db = FirebaseFirestore.getInstance();
                     FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                            .setTimestampsInSnapshotsEnabled(true)
                             .build();
-                    db.setFirestoreSettings(settings);
+                    mDb.setFirestoreSettings(settings);
 
-                    DocumentReference userRef = db.collection(getString(R.string.collection_users))
+                    DocumentReference userRef = mDb.collection(getString(R.string.collection_users))
                             .document(user.getUid());
 
                     userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                             if(task.isSuccessful()){
-                                Log.d(TAG, "onComplete: successfully set the user client.");
-                                User user_ = task.getResult().toObject(User.class);
-                                System.out.println("yadadada");
-                                System.out.println(user_);
-                                if(user_ == null)
+                                User user = task.getResult().toObject(User.class);
+                                System.out.println(user);
+                                if(user == null) // TODO maybe add another check for unique username
                                 {
                                     getUserName();
                                 }
                                 else {
-                                    ((UserClient) (getApplicationContext())).setUser(user_);
+                                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUser_id());
+                                    setCurrentUser(user);
                                     startMainActivity();
                                 }
                             }
                         }
                     });
-
-
-
                 } else {
                     // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
                     startAuthenticationActivity();
                 }
-                // ...
             }
         };
     }
 
     private void getUserName(){
-        Intent intent = new Intent(LoginActivity.this, UsernameActivity.class);
-        startActivityForResult(intent, GET_USERNAME_REQUEST);
+        setContentView(R.layout.activity_username);
+        mUsername = findViewById(R.id.input_username);
+        findViewById(R.id.btn_go).setOnClickListener(this);
+        hideSoftKeyboard();
     }
 
+    @Override
+    public void onClick(View v) {
+        showDialog();
+        if(v.getId() == R.id.btn_go) {
+            Log.d(TAG, "onClick: attempting to verify username.");
+            String userName = mUsername.getText().toString();
+            switch (checkUsername(userName)) {
+                case EMPTY:{
+                    Toast.makeText(LoginActivity.this,
+                            "Error message informing of empty username",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                }
+
+                case WRONG_FORMAT: {
+
+                    // TODO
+                    // maybe add another func for more specific error codes
+
+                    Toast.makeText(LoginActivity.this,
+                            "Error message informing of username containing wrong chars",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                default:
+                    checkExisting(userName);
+            }
+            hideDialog();
+        }
+    }
+
+    private int checkUsername(String userName)
+    {
+        if(isEmpty(userName))
+        {
+            return EMPTY;
+        }
+        if (!new UsernameValidator().validate(userName))
+        {
+            return WRONG_FORMAT;
+        }
+        return 0;
+    }
+
+    private void checkExisting(final String userName)
+    {
+        CollectionReference usersRef = mDb.collection("Users");
+        Query query = usersRef.whereEqualTo("username", userName);
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    if (task.getResult() != null) {
+                        for (DocumentSnapshot documentSnapshot : task.getResult()) {
+                            String user = documentSnapshot.getString("username");
+
+                            if (user.equals(userName)) {
+                                Log.d(TAG, "chcekExisting: User Exists");
+                                Toast.makeText(LoginActivity.this,
+                                        "checkExisting: This username is already taken",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        if(task.getResult().size() == 0 ){
+                            Log.d(TAG, "checkExisting: User chose a unique username");
+                            // SUCCESS
+                            User user = registerNewUser(userName);
+                            setCurrentUser(user);
+                            startMainActivity();
+                        }
+                    }
+                }
+            }
+        });
+    }
 
     private User registerNewUser(String userName){
         FirebaseUser cur_user = mAuth.getCurrentUser();
@@ -193,6 +276,14 @@ public class LoginActivity extends AppCompatActivity
 
         newUserRef.set(user);
         return user;
+    }
+
+    private void setCurrentUser(User user){
+        Toast.makeText(LoginActivity.this,
+                "Authenticated with: " + user.getEmail(),
+                Toast.LENGTH_SHORT).show();
+        ((UserClient) (getApplicationContext())).setUser(user);
+        Log.d(TAG, "setCurrentUser: successfully set the user client.");
     }
 
     @Override
